23fdeda84f2c1aaa26a10759f7ebdf63b94a241c
Zeus <aboode.aafaa2009@gmail.com>
2025-08-09 00:33:25 +0300

    Create user.model.js

---
+++ models/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    googleId: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    picture: { type: String }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
